dd532b5ca13156871a8fdc6021301e3326bc4ca6
Enda Lee <enda.lee@ittdublin.ie>
2016-12-16 15:07:36 +0000

    products in 2.sql

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,5 +1,19 @@
 INSERT INTO USER (username,name,password,role) VALUES ('admin', 'Alice', 'password', 'admin');
 INSERT INTO USER (username,name,password,role) VALUES ('manager', 'Bob', 'password', 'manager');
 INSERT INTO USER (username,name,password,role) VALUES ('customer', 'Charlie', 'password', 'customer');
-INSERT INTO category VALUES (1, 'T-Shirt');
-INSERT INTO category VALUES (2, 'Hoodie');
+
+
+INSERT INTO Category VALUES(1, 'T-Shirt');
+INSERT INTO Category VALUES(2, 'Hoodie');
+INSERT INTO category VALUES(3, 'Sweatshirt');
+INSERT INTO Category VALUES(4, 'Sweatpants');
+INSERT INTO Category VALUES(5, 'Tracksuit');
+
+
+INSERT INTO product VALUES(1, 'Grey/Green Hoodie', 2, 'Toxic Green alcantara 23 on back with signature face on hood. Black Rubber Embossed with white detailing on either side', 23, 60.00);
+INSERT INTO product VALUES(2, 'Vintage T-Shirt', 1, 'Rare vintage Hip Hop T-Shirt, limited edition', 0, 45.00);
+INSERT INTO product VALUES(4, 'Ouija T-Shirt', 1, 'Glow in the dark ouija planchette with reflective glass aperture', 25, 30);
+INSERT INTO product VALUES(3, 'Black/Green Hoodie', 2, 'Toxic Green alcantara 23 on back with signature face on hood. Black Rubber Embossed with white detailing on either side', 23, 60.00);
+INSERT INTO product VALUES(5, 'Norma Jean Hoodie', 2, 'In black with pink suede lettering', 14, 60);
+INSERT INTO product VALUES(6, 'Grey/Black/Pink Sweatshirt', 3, 'Lettering made from the reclaimed number plates of vehicles that have been involved in CAT B traffic accidents. In grey, with pink alcantara and tyre black rubber', 68, 25);
+INSERT INTO product VALUES(7, 'Grey/Black/Pink Sweatpants', 4, 'Lettering made from the reclaimed number plates of vehicles that have been involved in CAT B traffic accidents. In grey, with pink alcantara and tyre black rubber', 9, 15);
